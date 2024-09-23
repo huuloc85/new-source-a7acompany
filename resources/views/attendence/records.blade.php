25e2cb439b53f9f723b314d3ce5d2c1d7090de0c
huuloc85 <locnhphgcs210324@fpt.edu.vn>
2024-09-23 13:03:44 +0700

    update view layout attendance

--- resources/views/attendence/records.blade.php
+++ resources/views/attendence/records.blade.php
@@ -2,13 +2,22 @@
 
 @section('content')
     <style>
+        .form-control,
+        .form-select {
+            border-radius: 0.375rem;
+            font-size: 0.875rem;
+        }
+
         .form-control-search {
             padding-left: 2.5rem;
-            border-radius: 20px;
+            border-radius: 25px;
             border: 1px solid #ced4da;
             font-size: 0.875rem;
-            max-width: 400px;
-            width: 100%;
+            width: 50%
+        }
+
+        .position-relative {
+            position: relative;
         }
 
         .search-icon {
@@ -27,7 +36,25 @@
         input#search:focus::placeholder {
             color: transparent;
         }
+
+        @media (max-width: 768px) {
+
+            .mb-2,
+            .btn {
+                width: 100%;
+            }
+
+            .btn {
+                margin-bottom: 0.5rem;
+            }
+
+            .form-control-search {
+
+                width: 100%
+            }
+        }
     </style>
+
     <div class="row">
         <div class="col-12">
             <div class="card">
@@ -37,106 +64,111 @@
                             {{ \Carbon\Carbon::parse($currentMonth)->format('m-Y') }}</h4>
                     </div>
                 </div>
-                <div class="d-flex align-items-center my-2 ps-2">
-                    <button type="button" class="btn btn-primary btn-sm rounded-pill shadow-sm me-2" data-bs-toggle="modal"
-                        data-bs-target="#filterModal">
-                        Lọc Thông Tin
-                    </button>
-                    <a href="{{ route('admin.attendence.index') }}"
-                        class="btn btn-success btn-sm rounded-pill shadow-sm me-2">
-                        Bảng Lịch Sử Chấm Công
-                    </a>
-                    <div class="position-relative flex-grow-1 ms-2">
+                <div class="d-flex flex-wrap align-items-center my-2 ps-2 pe-2">
+                    <div class="mb-2 d-flex ps-2">
+                        <button type="button" class="btn btn-primary btn-sm rounded-pill shadow-sm me-2"
+                            data-bs-toggle="modal" data-bs-target="#filterModal">
+                            Lọc Thông Tin
+                        </button>
+                        <a href="{{ route('admin.attendence.index') }}"
+                            class="btn btn-success btn-sm rounded-pill shadow-sm">
+                            Bảng Lịch Sử Chấm Công
+                        </a>
+                    </div>
+                    <div class="mb-2 me-2 flex-grow-1 position-relative ps-2">
                         <input type="text" id="search" class="form-control form-control-search"
                             placeholder="Tìm kiếm theo tên nhân viên hoặc ngày chấm công">
                         <i class="search-icon fas fa-search"></i>
                     </div>
                 </div>
+
                 <div class="card-body">
-                    @if ($records->isEmpty())
-                        <p class="text-center">Hiện tại chưa có thông tin nào.</p>
-                    @else
-                        <input type="checkbox" id="filter_absent" name="filter_absent" value="1"
-                            {{ request('filter_absent') ? 'checked' : '' }}>
-                        Chỉ hiển thị những người quên chấm công
-                        <table id="attendanceTable" class="table table-hover mb-4">
-                            <thead class="text-center">
-                                <tr>
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
-                                        STT</th>
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
-                                        Mã Nhân Viên</th>
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
-                                        Tên Nhân Viên</th>
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
-                                        Ngày Chấm</th>
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
-                                        Ngày Trong Tuần</th>
+                    <div class="table-responsive">
+                        @if ($records->isEmpty())
+                            <p class="text-center">Hiện tại chưa có thông tin nào.</p>
+                        @else
+                            <input type="checkbox" id="filter_absent" name="filter_absent" value="1"
+                                {{ request('filter_absent') ? 'checked' : '' }}>
+                            Chỉ hiển thị những người quên chấm công
+                            <table id="attendanceTable" class="table table-hover mb-4">
+                                <thead class="text-center">
+                                    <tr>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                                            STT</th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                                            Mã Nhân Viên</th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                                            Tên Nhân Viên</th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                                            Ngày Chấm</th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                                            Ngày Trong Tuần</th>
 
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
-                                        Giờ Vào</th>
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
-                                        Giờ Ra</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center"
-                                        style="{{ is_null(request('time_filter')) || request('time_filter') === 'none' || in_array(request('time_filter'), ['qc_day', 'working_hours']) ? 'display: none;' : '' }}">
-                                        Ca Làm Việc
-                                    </th>
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
-                                        Tổng Giờ Làm Việc(H)</th>
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
-                                        Giờ Hành Chính(H) </th>
-                                    <th
-                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
-                                        Giờ Tăng Ca(H)</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($records as $record)
-                                    <tr class="text-center">
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $record->employee_code }}</td>
-                                        <td>{{ $record->employee ? $record->employee->name : 'Không xác định' }}</td>
-                                        <td>{{ \Carbon\Carbon::parse($record->date)->format('d-m-Y') }}</td>
-                                        <td>{{ $record->day_of_week }}</td>
-                                        <td class="{{ $record->time_in ? '' : 'text-danger' }}">
-                                            {{ $record->time_in ? \Carbon\Carbon::parse($record->time_in)->format('H:i:s') : 'Chưa chấm công vào' }}
-                                        </td>
-                                        <td class="{{ $record->time_out ? '' : 'text-danger' }}">
-                                            {{ $record->time_out ? \Carbon\Carbon::parse($record->time_out)->format('H:i:s') : 'Chưa chấm công ra' }}
-                                        </td>
-                                        <td
-                                            style="{{ request('time_filter') === null || request('time_filter') === 'none' || in_array(request('time_filter'), ['qc_day', 'working_hours']) ? 'display: none;' : '' }}">
-                                            <strong>{{ $record->shift === 'Đổi lịch đi làm' ? $record->shift : '' }}</strong>
-                                            {{ $record->shift !== 'Đổi lịch đi làm' ? $record->shift : '' }}
-                                        </td>
-
-                                        <td class="{{ $record->total_hours ? '' : 'text-danger' }}">
-                                            @if ($record->total_hours)
-                                                <strong>{{ $record->total_hours }}</strong>
-                                            @else
-                                                {{ 'Chấm công không đủ' }}
-                                            @endif
-                                        </td>
-                                        <td>
-                                            <strong>{{ $record->administrative_hours > 0 ? number_format($record->administrative_hours, 2) : '0' }}</strong>
-                                        </td>
-                                        <td>
-                                            <strong>{{ $record->overtime_hours > 0 ? number_format($record->overtime_hours, 2) : '0' }}</strong>
-                                        </td>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                                            Giờ Vào</th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                                            Giờ Ra</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center"
+                                            style="{{ is_null(request('time_filter')) || request('time_filter') === 'none' || in_array(request('time_filter'), ['qc_day', 'working_hours']) ? 'display: none;' : '' }}">
+                                            Ca Làm Việc
+                                        </th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                                            Tổng Giờ Làm Việc(H)</th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                                            Giờ Hành Chính(H) </th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                                            Giờ Tăng Ca(H)</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    @endif
+                                </thead>
+                                <tbody>
+                                    @foreach ($records as $record)
+                                        <tr class="text-center">
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $record->employee_code }}</td>
+                                            <td>{{ $record->employee ? $record->employee->name : 'Không xác định' }}</td>
+                                            <td>{{ \Carbon\Carbon::parse($record->date)->format('d-m-Y') }}</td>
+                                            <td>{{ $record->day_of_week }}</td>
+                                            <td class="{{ $record->time_in ? '' : 'text-danger' }}">
+                                                {{ $record->time_in ? \Carbon\Carbon::parse($record->time_in)->format('H:i:s') : 'Chưa chấm công vào' }}
+                                            </td>
+                                            <td class="{{ $record->time_out ? '' : 'text-danger' }}">
+                                                {{ $record->time_out ? \Carbon\Carbon::parse($record->time_out)->format('H:i:s') : 'Chưa chấm công ra' }}
+                                            </td>
+                                            <td
+                                                style="{{ request('time_filter') === null || request('time_filter') === 'none' || in_array(request('time_filter'), ['qc_day', 'working_hours']) ? 'display: none;' : '' }}">
+                                                <strong>{{ $record->shift === 'Đổi lịch đi làm' ? $record->shift : '' }}</strong>
+                                                {{ $record->shift !== 'Đổi lịch đi làm' ? $record->shift : '' }}
+                                            </td>
+
+                                            <td class="{{ $record->total_hours ? '' : 'text-danger' }}">
+                                                @if ($record->total_hours)
+                                                    <strong>{{ $record->total_hours }}</strong>
+                                                @else
+                                                    {{ 'Chấm công không đủ' }}
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <strong>{{ $record->administrative_hours > 0 ? number_format($record->administrative_hours, 2) : '0' }}</strong>
+                                            </td>
+                                            <td>
+                                                <strong>{{ $record->overtime_hours > 0 ? number_format($record->overtime_hours, 2) : '0' }}</strong>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
