5b8f42c129e04a157b041979827613c48edd5335
AHuyHarryIT <ahgamingofficial03@gmail.com>
2025-01-24 10:09:37 +0700

    refactor: Material plan page

--- resources/views/material/index.blade.php
+++ resources/views/material/index.blade.php
@@ -2,40 +2,24 @@
 
 @section('content')
     <div class="row">
-        <div class="col-sm-12">
+        <div class="col-12">
             <div class="card">
-                <div
-                    class="card-header p-1 position-relative mt-n1 mx-1 no-print"
-                >
-                    <div class="border-radius-lg ps-2 pt-4 pb-3">
-                        <h4 class="card-title mb-0">Danh Sách Nguyên Liệu</h4>
-                    </div>
-                </div>
+                <div class="card-header"><h4>Danh Sách Nguyên Liệu</h4></div>
                 <div class="card-body">
                     <table class="table table-bordered">
-                        <thead>
-                            <tr class="text-center">
-                                <th
-                                    class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center"
-                                >
-                                    STT
-                                </th>
-                                <th
-                                    class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center"
-                                >
-                                    Mã Nguyên Liệu
-                                </th>
-                                <th
-                                    class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center"
-                                >
-                                    Tổng Số Lượng
-                                </th>
+                        <thead class="text-center text-uppercase">
+                            <tr>
+                                <th>STT</th>
+                                <th>Mã Nguyên Liệu</th>
+                                <th>Tổng Số Lượng</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody class="text-center">
                             @foreach ($materials as $index => $material)
-                                <tr class="text-center">
-                                    <td>{{ $loop->iteration }}</td>
+                                <tr>
+                                    <td class="fw-bold">
+                                        {{ $loop->iteration }}
+                                    </td>
                                     <td>{{ $material['material'] }}</td>
                                     <td>
                                         {{ number_format($material['total_quantity'], 2) }}
