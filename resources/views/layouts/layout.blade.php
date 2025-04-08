82a657104e112b303e76620b67e973c2844f47ed
huuloc85 <locnhphgcs210324@fpt.edu.vn>
2025-04-08 16:35:10 +0700

    add vite.js

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -92,6 +92,7 @@
         />
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         @vite('resources/js/pusher.js')
+        @vite('resources/js/scan.js')
 
         <!-- Styling css -->
         <style class="fslightbox-styles">
