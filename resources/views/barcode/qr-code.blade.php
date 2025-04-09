d7743dd9a5c3e98300db770e6766ee3737b9f533
huuloc85 <locnhphgcs210324@fpt.edu.vn>
2025-04-09 07:33:07 +0700

    config vite js

--- resources/views/barcode/qr-code.blade.php
+++ resources/views/barcode/qr-code.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.'.$layout)
+
 @section('styles')
     <link rel="stylesheet" href="{{ asset('assets/css/scan-qrcode.css') }}" />
 @endsection
@@ -36,7 +37,7 @@
                                     </p>
                                     <p class="fs-6">
                                         <span class="fw-bold">Lưu ý:</span>
-                                        Khoảng nghĩ giữa 2 lần quét mã thành
+                                        Khoảng nghỉ giữa 2 lần quét mã thành
                                         công là
                                         <span class="text-danger">5 giây</span>
                                         .
@@ -48,7 +49,7 @@
                             <div class="col-12">
                                 <div
                                     id="reader"
-                                    data-url="{{ route('admin.barcode.check') }}"
+                                    data-url="{{ route('admin.barcode.checkQr') }}"
                                     class="viewport"
                                 ></div>
                             </div>
@@ -71,95 +72,5 @@
 @endsection
 
 @section('scripts')
-    <script>
-        import { Html5Qrcode } from 'html5-qrcode';
-
-        document.addEventListener('DOMContentLoaded', function () {
-            let startApi = true;
-
-            const reader = document.getElementById('reader');
-            const url = reader.dataset.url;
-
-            const html5QrCode = new Html5Qrcode('reader');
-
-            const onScanSuccess = (decodedText, decodedResult) => {
-                if (
-                    decodedText &&
-                    decodedText !== '*!' &&
-                    decodedText !== "U8'48*("
-                ) {
-                    console.log('✅ Mã quét:', decodedText);
-
-                    if (startApi) {
-                        fetch(url, {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'X-CSRF-TOKEN': document
-                                    .querySelector('meta[name="csrf-token"]')
-                                    .getAttribute('content'),
-                            },
-                            body: JSON.stringify({
-                                barcode: decodedText,
-                            }),
-                        })
-                            .then((res) => res.json())
-                            .then((response) => {
-                                switch (response.status) {
-                                    case 200:
-                                        alert(
-                                            '✅ Cập nhật thành công. Đợi 5 giây...',
-                                        );
-                                        break;
-                                    case 400:
-                                        alert('⚠️ Mã đã được quét.');
-                                        break;
-                                    case 404:
-                                        alert('❌ Không tìm thấy sản phẩm.');
-                                        break;
-                                    default:
-                                        alert('⚠️ Lỗi không xác định.');
-                                        break;
-                                }
-                            })
-                            .catch((err) => console.error('Lỗi gửi mã:', err));
-
-                        startApi = false;
-                        setTimeout(() => {
-                            startApi = true;
-                        }, 5000);
-                    }
-                }
-            };
-
-            Html5Qrcode.getCameras()
-                .then((devices) => {
-                    if (devices && devices.length) {
-                        const cameraId = devices[0].id;
-                        html5QrCode
-                            .start(
-                                {
-                                    deviceId: {
-                                        exact: cameraId,
-                                    },
-                                },
-                                {
-                                    fps: 10,
-                                    qrbox: 250,
-                                },
-                                onScanSuccess,
-                            )
-                            .catch((err) => {
-                                console.error(
-                                    'Không thể khởi động camera:',
-                                    err,
-                                );
-                            });
-                    }
-                })
-                .catch((err) => {
-                    console.error('Không thể truy cập camera:', err);
-                });
-        });
-    </script>
+    @vite(['resources/js/scan.js'])
 @endsection
