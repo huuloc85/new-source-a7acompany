ad5922ebd5b94c0f1c09fa7c2da03cad597beeba
huuloc85 <locnhphgcs210324@fpt.edu.vn>
2025-08-19 11:48:30 +0700

    fix: correct spacing in query conditions for Employee model scopes

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 
 class Employee extends Authenticatable
 {
